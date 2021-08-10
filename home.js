56c1e2f419ae8d62fb06a1c40692663a37ae8cde
edwardTunkl <eddytunkl@googlemail.com>
2021-08-10 21:03:02 +0200

    Creation of copied clone

---
+++ home.js
@@ -0,0 +1,16 @@
+var myDate = new Date();
+var hrs = myDate.getHours();
+
+var greet;
+
+if (hrs < 12) greet = "Good Morning";
+else if (hrs >= 12 && hrs <= 17) greet = "Good Afternoon";
+else if (hrs >= 17 && hrs <= 24) greet = "Good Evening";
+
+document.getElementById("greet").innerHTML = "<b>" + greet;
+
+const changeSidebarSelected = function(event){
+    let currentlySelectedItem = document.querySelector('.selctedSidebarItem')
+    currentlySelectedItem.classList.remove('selctedSidebarItem')
+    event.target.classList.add('selctedSidebarItem')
+  }
